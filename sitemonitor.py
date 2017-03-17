ace6582b85837d69672bc3a8fd54edaa0b9a8003
Jay Sanders <WyldPhyr@users.noreply.github.com>
2017-03-17 10:26:56 -0700

    Add User Input

--- sitemonitor.py
+++ sitemonitor.py
@@ -4,18 +4,19 @@ import time
 import smtplib
 
 while True:
-	url = "http://reddit.com/"
+	uri = raw_input('Please enter the URI of the site you wish to monitor: ")
+	email = raw_input("Please enter your email address to be notified when the site changes: ")
 	headers = {'User-Agent': 'Chrome/56.0.2924.87'}
 	response = requests.get(url, headers=headers)
 	soup = BeautifulSoup(response.text, "lxml")
-
+#      Change the "Reddit" value, I'll leave that for someone else ;)
 	if str(soup).find("Reddit") == -1:
 		time.sleep(60)
 		continue
 	else:
 		msg = 'REDDIT UPDATED!'
 		fromaddr = 'justthings4schcool@gmail.com'
-		toaddr  = ['thanhtrinh12122010@gmail.com','davidtrieu6@gmail.com', 'gameguy47@gmail.com', 'francis.c.phan@gmail.com']
+		toaddr  = [email]
 
 		server = smtplib.SMTP("smtp.gmail.com:587")
 		server.starttls()
